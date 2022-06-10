a151e45da691f0a86dea2bb1d9a4cecd16e74b06
63rabbits goodman <63rabbitsgoodman@gmail.com>
2022-06-10 14:06:08 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ sandbox.ts
@@ -0,0 +1,16 @@
+//% block
+//% block.loc.ja="サンドボックス"
+//% weight=100
+//% color=#0fbc11
+//% icon="\uf0c3"
+namespace sandbox {
+
+    //% block
+    export function sample() {
+        
+    }
+
+
+
+
+}
