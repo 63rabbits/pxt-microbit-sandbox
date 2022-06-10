268171038ba2c067f4b851baee5d0a240b214359
63rabbits goodman <63rabbitsgoodman@gmail.com>
2022-06-11 04:10:12 +0900

    更新 pxt.json, main.blocks, sandbox.ts

--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,10 @@
-//% block
-//% block.loc.ja="サンドボックス"
 //% weight=100
 //% color=#0fbc11
 //% icon="\uf0c3"
 namespace sandbox {
 
     //% block
-    export function sample() {
+    export function sample(arg1: number, arg2: number) {
         
     }
 
